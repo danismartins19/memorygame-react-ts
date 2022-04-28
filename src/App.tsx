562499bfba6028dde2b024e808355ffac1fedaf0
Daniel Martins <danidiamoo1@gmail.com>
2022-04-27 21:25:54 -0300

    Labels e botao criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import * as C from './App.styles';
 import LogoMemory from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
+
+const resetAndCreateGrid = () => {
+
+}
 
 export const App = () => {
   return (
@@ -10,10 +17,11 @@ export const App = () => {
         </C.LogoLink>
 
         <C.InfoArea>
-          ...
+          <InfoItem label='Tempo' value='00:00'></InfoItem>
+          <InfoItem label='Movimentos' value='0'></InfoItem>
         </C.InfoArea>
 
-        <button>Reiniciar</button>
+        <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid}></Button>
 
       </C.Info>
       <C.GridArea>
