4595ef29e1e99a12f27283eb3af13aa90b73d608
Daniel Martins <danidiamoo1@gmail.com>
2022-04-28 21:41:41 -0300

    criacao do componente do item da grid

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,6 +1,8 @@
 import { GridItemType } from '../../types/GridItemType';
 import * as C from './styles';
 import b7svg from '../../svgs/b7.svg';
+import { items } from '../../data/items';
+
 
 type Props ={
     item: GridItemType,
@@ -12,6 +14,10 @@ export const GridItem = ({item, onClick}: Props) =>{
        <C.Container onClick={onClick}>
            {!item.permanent && !item.shown &&
                 <C.Icon src= {b7svg} alt=""></C.Icon>
+           }
+
+           {(item.permanent || item.shown )&& item.item !== null &&
+                <C.Icon src={items[item.item].icon} alt=''></C.Icon>
            }
        </C.Container>
     )
